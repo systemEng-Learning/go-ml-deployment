9fc74c4346b5ac4f3e90b057f9e0f91488b54427
steveoni <steohenoni2@gmail.com>
2025-04-10 18:26:55 +0100

    update softmax

--- tensor/softmax.go
+++ tensor/softmax.go
@@ -6,20 +6,43 @@ import (
 )
 
 func (t *Tensor) SoftmaxInPlace() error {
-	if len(t.Shape) != 2 || t.DType != Double {
+	if len(t.Shape) != 2 || t.DType != Double || t.DType != Float {
 		return errors.ErrUnsupported
 	}
-	var sum float64
-	for i := range t.Shape[0] {
-		sum = 0
-		for j := range t.Shape[1] {
-			t.DoubleData[i*t.Shape[1]+j] = math.Exp(t.DoubleData[i*t.Shape[1]+j])
-			sum += t.DoubleData[i*t.Shape[1]+j]
-		}
 
-		for j := range t.Shape[1] {
-			t.DoubleData[i*t.Shape[1]+j] = t.DoubleData[i*t.Shape[1]+j] / sum
-		}
+	switch t.DType {
+	case Float:
+		SoftMax(t.Shape, t.FloatData)
+	case Double:
+		SoftMax(t.Shape, t.DoubleData)
 	}
 	return nil
+}
+
+type Float32_64 interface {
+	float32 | float64
+}
+
+func SoftMax[T Float32_64](shape []int, data []T) {
+	for i :=0; i < shape[0]; i++ {
+		start := i * shape[1]
+		end := start + shape[1]
+		row := data[start:end]
+		max := row[0]
+		for _, val := range row {
+			if val > max {
+				max = val
+			}
+		}
+		var sum T
+		for j := range row {
+			row[j] = T(math.Exp(float64(row[j] - max)))
+			sum += row[j]
+		}
+		for j := range row {
+			if sum != 0 {
+				row[j] /= sum
+			}
+		}
+	}
 }
